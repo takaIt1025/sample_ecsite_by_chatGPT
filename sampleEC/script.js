32ea84314d9231d97366654d924c30f3919ae0d3
TakayaIto <tky.ar2013@gmail.com>
2023-02-23 15:54:42 +0900

    ChatGPTとのやりとりでECサイトのサンプルを作ってみた

---
+++ sampleEC/script.js
@@ -0,0 +1,14 @@
+
+function addToCart() {
+  // カートに商品を追加する処理を実装する
+  // ...
+  
+  alert("カートに商品を追加しました！");
+}
+
+function deleteCart() {
+    // カート内の商品を削除
+    document.getElementById("cart-items").innerHTML = "";
+    alert("カート内の商品を削除しました！");
+    
+}
